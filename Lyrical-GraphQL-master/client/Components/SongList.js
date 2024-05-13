0dd61421df97f9121655549a82c49bfb42e60400
Akul Sareen <sareen.akul@gmail.com>
2024-05-13 18:47:06 -0400

    Added the SongList presentation feature

--- Lyrical-GraphQL-master/client/Components/SongList.js
+++ Lyrical-GraphQL-master/client/Components/SongList.js
@@ -1,9 +1,40 @@
 import React, {Component} from "react";
+import gql from "graphql-tag";
+import {graphql} from "react-apollo";
 
 class SongList extends Component{
+
+    renderSongs(){
+        return this.props.data.songs.map(song => {
+            return(
+                <li key={song.id} className="collection-item">{song.title}</li>
+            );
+        });
+    }
+
+
     render(){
-        return(<div>SongList</div>);
-    };
+        if(this.props.data.loading){
+            return(
+                <div>Loading...</div>
+            );
+        }
+
+        return(
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
+        );
+    }
 }
 
-export default SongList;
+const query = gql`
+    {
+        songs{
+            id
+            title
+        }
+    }
+`;
+
+export default graphql(query)(SongList);
