97436a4f5cc4316c856a41417cf202243b9d1f40
Akul Sareen <sareen.akul@gmail.com>
2024-05-13 21:06:07 -0400

    Used query variables to integrate mutations

---
+++ Lyrical-GraphQL-master/client/Components/SongCreate.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import gql from "graphql-tag";
+import {graphql} from "react-apollo";
+
+class SongCreate extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {title: ''};
+    }
+
+    onSubmit(event){
+        event.preventDefault();
+        this.props.mutate({
+            variables: {
+                title: this.state.title
+            }
+        });
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h3>Create a song</h3>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title: </label>
+                    <input onChange={event => this.setState({title: event.target.value})}
+                    value={this.state.title}/>
+                </form>
+            </div>
+        );
+    }
+}
+
+const mutation = gql`
+    mutation AddSong($title: String){
+        addSong(title: $title){
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
--- Lyrical-GraphQL-master/client/index.js
+++ Lyrical-GraphQL-master/client/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route , hashHistory, IndexRoute} from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList from './Components/SongList';
+import App from "./Components/App";
+import SongCreate from './Components/SongCreate';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return(
     <ApolloProvider client={client}>
-      <SongList/>
+      <Router history = {hashHistory}>
+        <Route path = "/" component = {App}>
+          <IndexRoute component={SongList}/>
+          <Route path="song/new" component={SongCreate}/>
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
