3a6e712643fc25d38f17f50a14f3a2df3f4a2791
Akul Sareen <sareen.akul@gmail.com>
2024-05-14 06:59:59 -0400

    Added the delete feature based of the delete mutation

--- Lyrical-GraphQL-master/client/Components/SongList.js
+++ Lyrical-GraphQL-master/client/Components/SongList.js
@@ -2,13 +2,29 @@ import React, {Component} from "react";
 import gql from "graphql-tag";
 import {graphql} from "react-apollo";
 import { Link } from "react-router";
+import "../style/style.css";
 
 class SongList extends Component{
+
+    handleDelete(event, songId){
+        event.preventDefault();
+        this.props.mutate({
+            variables:{
+                id: songId
+            },
+            refetchQueries: [{query: query}]
+        });
+    }
 
     renderSongs(){
         return this.props.data.songs.map(song => {
             return(
-                <li key={song.id} className="collection-item">{song.title}</li>
+                <li key={song.id} className="collection-item">
+                    <div className="songItem">
+                        {song.title}
+                        <button className="delete" onClick={(event)=> this.handleDelete(event ,song.id)}>Delete</button>
+                    </div>
+                </li>
             );
         });
     }
@@ -43,4 +59,12 @@ const query = gql`
     }
 `;
 
-export default graphql(query)(SongList);
+const deleteMutation = gql`
+    mutation DeleteSong($id: ID!){
+        deleteSong(id: $id){
+            id
+        }
+    }
+`;
+
+export default graphql(deleteMutation)(graphql(query)(SongList));
