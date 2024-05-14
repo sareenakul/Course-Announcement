022b7b2b388c9db4fe8d62235f48b51bd312a180
Akul Sareen <sareen.akul@gmail.com>
2024-05-13 21:17:18 -0400

    Added Link from react router

--- Lyrical-GraphQL-master/client/Components/SongCreate.js
+++ Lyrical-GraphQL-master/client/Components/SongCreate.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import gql from "graphql-tag";
 import {graphql} from "react-apollo";
+import { Link } from "react-router";
 
 class SongCreate extends Component{
     constructor(props){
@@ -22,6 +23,7 @@ class SongCreate extends Component{
     render(){
         return(
             <div>
+                <Link to="/">Back</Link>
                 <h3>Create a song</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title: </label>
