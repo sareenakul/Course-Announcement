e7c5edcf3e44c0e1f4e79c41457e440debf58e8a
Akul Sareen <sareen.akul@gmail.com>
2024-05-14 05:01:08 -0400

    used hashHistory

--- Lyrical-GraphQL-master/client/Components/SongCreate.js
+++ Lyrical-GraphQL-master/client/Components/SongCreate.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import gql from "graphql-tag";
 import {graphql} from "react-apollo";
-import { Link } from "react-router";
+import { Link, hashHistory } from "react-router";
 
 class SongCreate extends Component{
     constructor(props){
@@ -16,7 +16,7 @@ class SongCreate extends Component{
             variables: {
                 title: this.state.title
             }
-        });
+        }).then(() => hashHistory.push('/'));
     }
 
 
